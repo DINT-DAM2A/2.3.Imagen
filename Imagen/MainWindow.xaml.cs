8814c27110c499400abcf1725b947a714ead6e1c
Alex <oleksandr.veretennykov@iesdoctorbalmis.com>
2021-09-29 10:45:01 +0200

    Acabado

--- Imagen/MainWindow.xaml.cs
+++ Imagen/MainWindow.xaml.cs
@@ -13,31 +13,31 @@ namespace Imagen
             InitializeComponent();
         }
 
-        private void OpacidadAlta(object sender, RoutedEventArgs e)
+        private void OpacidadAlta_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Opacity = 1;
         }
-        private void OpacidadMedia(object sender, RoutedEventArgs e)
+        private void OpacidadMedia_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Opacity = 0.6;
         }
-        private void OpacidadBaja(object sender, RoutedEventArgs e)
+        private void OpacidadBaja_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Opacity = 0.3;
         }
-        private void AjusteRelleno(object sender, RoutedEventArgs e)
+        private void AjusteRelleno_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Stretch = Stretch.Fill;
         }
-        private void AjusteUniforme(object sender, RoutedEventArgs e)
+        private void AjusteUniforme_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Stretch = Stretch.Uniform;
         }
-        private void AjusteRellenoUniforme(object sender, RoutedEventArgs e)
+        private void AjusteRellenoUniforme_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Stretch = Stretch.UniformToFill;
         }
-        private void AjusteNone(object sender, RoutedEventArgs e)
+        private void AjusteNone_Cheched(object sender, RoutedEventArgs e)
         {
             Imagen.Stretch = Stretch.None;
         }
